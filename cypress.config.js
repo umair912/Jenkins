4b1a1ac2d23c42b15881a33a30bc2a42e15c0540
usmanmana <Muhammad.Usman@alchemyglobalsolutions.com>
2022-08-02 14:44:10 +0500

    Jenkins file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      reporter: 'mochawesome'
 
       // implement node event listeners here
     },
